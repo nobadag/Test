7b5090efaf2df0cc535c59b058e9347eb5ec4a55
nobadag <nobaan.dag@gmail.com>
2018-12-15 15:04:00 +0900

    平均点を演算し、表示するプログラムを追加。

--- Test.java
+++ Test.java
@@ -12,13 +12,18 @@ class Test
     int elm = Integer.parseInt(str);
 
     int[] sub = new int[elm];
+    int total = 0;
 
     for(int i = 0; i < elm; i++){
       do{
-        System.out.print("教科" + (i + 1) + "の点数を入力してください");
+        System.out.print("教科" + (i + 1) + "の点数を入力してください。");
         str = br.readLine();
         sub[i] = Integer.parseInt(str);
       } while(sub[i] < 0 || sub[i] > 100);
+      total += sub[i];
     }
+    double ave = total / elm;
+
+    System.out.println("平均点は" + ave + "点です。");
   }
 }
