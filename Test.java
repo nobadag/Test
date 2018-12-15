cabf8fde394ffa533a40ea9a44586a120f30312b
nobadag <nobaan.dag@gmail.com>
2018-12-15 12:45:26 +0900

    テストの教科数を入力させるプログラムを追加。

--- Test.java
+++ Test.java
@@ -4,6 +4,11 @@ class Test
 {
   public static void main(String[] args) throws IOException
   {
-    
+    System.out.print("テストの教科数を入力してください。");
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String str = br.readLine();
+    int elm = Integer.parseInt(str);
   }
 }
