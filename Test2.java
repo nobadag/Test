4c594dcc8a6f83a6ea8d74f2908ac64b6a5010a6
nobadag <nobaan.dag@gmail.com>
2018-12-19 18:40:58 +0900

    Userクラスとそのオブジェクトを人数分用意するプログラムを追加。

--- Test2.java
+++ Test2.java
@@ -1,9 +1,24 @@
 import java.io.*;
 
+class User
+{
+  private String name;
+  public int[] score;
+}
+
 class Test2
 {
   public static void main(String[] args) throws IOException
   {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    System.out.print(" l”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B");
+    String str = br.readLine();
+    int number = Integer.parseInt(str);
+    User[] users;
+    users = new User[number];
+    for(int i = 0; i < number; i++){
+      users[i] = new User();
+    }
   }
 }
